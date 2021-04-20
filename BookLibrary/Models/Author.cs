62872bb81aa5408fd334e8ad713a1ead10592312
mateuszczapla@gmail.com <mateuszczapla@gmail.com>
2021-04-20 20:08:33 +0200

    Definicja modelu autora i książki.

---
+++ BookLibrary/Models/Author.cs
@@ -0,0 +1,22 @@
+﻿namespace BookLibrary.Models
+{
+    /// <summary>
+    /// The main Author class.
+    /// </summary>
+    public class Author
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public Author(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ BookLibrary/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace BookLibrary.Models
+{
+    /// <summary>
+    /// The main Book class.
+    /// </summary>
+    public class Book
+    {
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+        }
+
+        private short year;
+        public short Year
+        {
+            get
+            {
+                return year;
+            }
+        }
+
+        public Book(string title, short year)
+        {
+            this.title = title;
+            this.year = year;
+        }
+    }
+}
