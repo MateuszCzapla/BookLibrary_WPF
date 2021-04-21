e894b19d577260f75dcbb5b171ef97bcad098bf5
mateuszczapla@gmail.com <mateuszczapla@gmail.com>
2021-04-21 22:32:30 +0200

    Przeniesienie sampli do osobnej klasy.

--- BookLibrary/Other/DatabaseOperations.cs
+++ BookLibrary/Other/DatabaseOperations.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Microsoft.Data.Sqlite;
-
-using System.Collections.Generic;
-using System.Threading;
 
 namespace BookLibrary.Other
 {
@@ -40,54 +36,12 @@ namespace BookLibrary.Other
                 command.ExecuteNonQuery();
             }
 
-            DatabaseOperations.FillSampleData();
+            DBSampleDataOperations.FillSampleData();
         }
 
         public static void DatabaseSync()
         {
 
-        }
-
-        private static void FillSampleData()
-        {
-            using (var connection = new SqliteConnection("Data Source=" + dbName))
-            {
-                DateTime unixEpoch = new DateTime(1970, 1, 1);
-                List<string> insertsList = new List<string>();
-                insertsList.Add(@"INSERT INTO author(name, timestamp) VALUES('Jacek Matulewski', $timeStamp);");
-                insertsList.Add(@"INSERT INTO author(name, timestamp) VALUES('Peter Bell', $timeStamp);");
-                insertsList.Add(@"INSERT INTO author(name, timestamp) VALUES('Brent Beer', $timeStamp);");
-                insertsList.Add(@"INSERT INTO author(name, timestamp) VALUES('Michał Włodarczyk', $timeStamp);");
-                insertsList.Add(@"INSERT INTO author(name, timestamp) VALUES('Krzysztof Rychlicki-Kicior', $timeStamp);");
-
-                connection.Open();
-
-                var command = connection.CreateCommand();
-                /*command.CommandText =
-                @"
-                    INSERT INTO author(name, timestamp) VALUES('Jacek Matulewski', $timeStamp);
-                    INSERT INTO author(name, timestamp) VALUES('Peter Bell', $timeStamp);
-                    INSERT INTO author(name, timestamp) VALUES('Brent Beer', $timeStamp);
-                    INSERT INTO author(name, timestamp) VALUES('Michał Włodarczyk', $timeStamp);
-                    INSERT INTO author(name, timestamp) VALUES('Krzysztof Rychlicki-Kicior', $timeStamp);
-                ";
-                command.Parameters.AddWithValue("$timeStamp", (DateTime.UtcNow - unixEpoch).TotalSeconds);
-                command.ExecuteNonQuery();
-                 */
-
-                command.CommandText = @"INSERT INTO author(name, timestamp) VALUES('Jacek Matulewski', $timeStamp);";
-                command.Parameters.AddWithValue("$timeStamp", (DateTime.UtcNow - unixEpoch).TotalSeconds);
-                command.ExecuteNonQuery();
-
-                foreach (string insert in insertsList)
-                {
-                    command = connection.CreateCommand();
-                    command.CommandText = insert;
-                    command.Parameters.AddWithValue("$timeStamp", (DateTime.UtcNow - unixEpoch).TotalSeconds);
-                    command.ExecuteNonQuery();
-                    Thread.Sleep(1000);
-                }
-            }
         }
     }
 }
