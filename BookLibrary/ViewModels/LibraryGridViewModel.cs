82e6b1101670209ad1104c81220b0005ab0826f8
mateuszczapla@gmail.com <mateuszczapla@gmail.com>
2021-04-22 22:45:26 +0200

    Zmiany w widoku modelu

--- BookLibrary/ViewModels/LibraryGridViewModel.cs
+++ BookLibrary/ViewModels/LibraryGridViewModel.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace BookLibrary.ViewModels
 {
-    public class LibraryGridViewModel
+    public class LibraryGridViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        protected void OnPropertyChanged(params string[] propertyNames)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (string propertyName in propertyNames)
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
