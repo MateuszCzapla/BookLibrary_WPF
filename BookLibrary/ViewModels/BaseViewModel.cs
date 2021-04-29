8b24a4c14bdefbeb8271ff69f9c4e37cd87a853f
mateuszczapla@gmail.com <mateuszczapla@gmail.com>
2021-04-29 23:26:20 +0200

    Rozwinięcie logiki dla głównego menu.

---
+++ BookLibrary/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace BookLibrary.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        protected Mode mode;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BaseViewModel()
+        {
+            this.mode = Mode.Book;
+        }
+    }
+
+    public enum Mode
+    {
+        Author,
+        Book,
+        Reader
+    }
+}
---
+++ BookLibrary/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace BookLibrary.ViewModels
+{
+    public class MainMenuViewModel : BaseViewModel
+    {
+        //private ICommand lastPageCommand;
+        /*public ICommand LastPageCommand
+        {
+            get
+            {
+                if (lastPageCommand == null)
+                {
+                    lastPageCommand = new RelayCommand(
+                        argument =>
+                        {
+                            firstRow = totalRowsCount - rowsCount;
+                        },
+                        argument => true);
+                }
+                return lastPageCommand;
+            }
+        }*/
+
+        public MainMenuViewModel()
+        {
+
+        }
+    }
+}
