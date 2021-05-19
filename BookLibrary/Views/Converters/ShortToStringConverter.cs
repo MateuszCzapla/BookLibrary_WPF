11120f59fc56f3d3e89d02a28b66f24b62b70d91
mateuszczapla@gmail.com <mateuszczapla@gmail.com>
2021-05-19 22:13:00 +0200

    Poprawki w konwerterze ShortToStringConverter

--- BookLibrary/Views/Converters/ShortToStringConverter.cs
+++ BookLibrary/Views/Converters/ShortToStringConverter.cs
@@ -3,18 +3,16 @@ using System.Windows.Data;
 
 namespace BookLibrary.Views.Converters
 {
-    public class StringToShortConverter : IValueConverter
+    public class ShortToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            //return value;
-            return System.Convert.ToInt16(value);
+            if ((short)value == 0) return String.Empty;
+            return value;
         }
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            //if ((short)value == 0) return String.Empty;
-            if ((short)value == 0) return String.Empty;
-            return value.ToString();
+            return System.Convert.ToInt16(value);
         }
     }
 }
