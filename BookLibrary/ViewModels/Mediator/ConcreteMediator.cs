87a04221abb7221ee62270064dc180685ed92f44
mateuszczapla@gmail.com <mateuszczapla@gmail.com>
2021-05-23 14:55:17 +0200

    Przenoszenie funkcjonalności Result to osobnego UserControl.

--- BookLibrary/ViewModels/Mediator/ConcreteMediator.cs
+++ BookLibrary/ViewModels/Mediator/ConcreteMediator.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using BookLibrary.ViewModels.Mediator;
-using BookLibrary.ViewModels;
 
 namespace BookLibrary.ViewModels.Mediator
 {
     class ConcreteMediator : IMediator
     {
         private QueryBookViewModel queryBookViewModel;
-        private ResultViewModel resultViewModel;
+        private OldResultViewModel resultViewModel;
 
-        public ConcreteMediator(QueryBookViewModel queryBookViewModel, ResultViewModel resultViewModel)
+        public ConcreteMediator(QueryBookViewModel queryBookViewModel, OldResultViewModel resultViewModel)
         {
             this.queryBookViewModel = queryBookViewModel;
             this.queryBookViewModel.SetMediator(this);
